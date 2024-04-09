a7e808c816dfb4680286ec3cdcfbe8047db1869a
Laísa Alves <124175402+laisa-alves@users.noreply.github.com>
2024-04-09 17:57:51 -0300

    Define autenticação junto com a api delivery

--- src/auth.ts
+++ src/auth.ts
@@ -1,3 +1,5 @@
+import { storage } from "./storage"
+
 async function signIn(email, password, onSuccess) {
     console.log("Will sign in...")
     const body = {
@@ -15,9 +17,38 @@ async function signIn(email, password, onSuccess) {
         },
         body: JSON.stringify(body)
     })
-    response.json().then((json) => onSuccess())
+    response.json().then((json) => {
+        storage.store('token', json.token)
+        storage.store('email', json.email)
+        onSuccess()
+    })
+}
+
+function loggedIn() {
+    return Boolean(storage.get('token'))
+}
+
+function currentUser() {
+    if(!loggedIn()) {
+        return null
+    }
+    return {
+        email: storage.get('email')
+    }
+}
+
+function signOut(andThen = null) {
+    storage.remove('token')
+    storage.remove('email')
+
+    if(typeof(andThen) == 'function') {
+        andThen()
+    }
 }
 
 export const auth = {
-    signIn: signIn
+    signIn,
+    loggedIn,
+    currentUser,
+    signOut
 }
