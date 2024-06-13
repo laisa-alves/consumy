90f5e0e74f2327a5362d06825e3630c81d2967c8
Laísa Alves <124175402+laisa-alves@users.noreply.github.com>
2024-06-13 18:54:10 -0300

    Adiciona o pinia, cria rota para home e cria um store para controle da sidebar

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import SignIn from '../components/SignIn.vue'
 import WelcomeView from '../views/WelcomeView.vue'
+import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/welcome',
       name: 'welcome',
       component: WelcomeView
     },
