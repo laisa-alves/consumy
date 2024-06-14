8d57e4acce5a2f02a0a0b5a23fc128d56c798a4b
Laísa Alves <124175402+laisa-alves@users.noreply.github.com>
2024-06-14 19:58:31 -0300

    Instala a biblioteca FormKit para lidar com formulários na aplicação

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import './assets/tailwind.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { plugin, defaultConfig } from '@formkit/vue'
+import config from '../formkit.config'
 import App from './App.vue'
 import router from './router'
 
@@ -10,6 +12,7 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(plugin, defaultConfig(config))
 
 app.config.globalProperties.$formatCurrency = (value: number) => {
   if (!value) return 'R$ 0,00'
