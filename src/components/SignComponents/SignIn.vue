28009082eae27d63b04e7614ba694208188b9482
Laísa Alves <124175402+laisa-alves@users.noreply.github.com>
2024-06-15 13:20:37 -0300

    Adiciona formulários para login e cadastro

--- src/components/SignComponents/SignIn.vue
+++ src/components/SignComponents/SignIn.vue
@@ -1,4 +1,6 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { FormKit } from '@formkit/vue'
+</script>
 
 <template>
   <!-- Form send error -->
@@ -7,5 +9,28 @@
     <p class="mt-2 text-sm text-red-700">
       Por favor, verifique sua conexão com a internet e tente novamente mais tarde.
     </p>
+  </div>
+
+  <!-- Form -->
+  <div>
+    <FormKit type="form">
+      <FormKit
+        type="email"
+        name="email"
+        label="E-mail"
+        validation="required|email"
+        validation-visibility="blur"
+      />
+      <FormKit
+        type="password"
+        name="password"
+        label="Senha"
+        value="super-secret"
+        validation="required"
+        validation-visibility="blur"
+      />
+
+      <FormKit type="checkbox" name="remember" label="Lembre-se de mim" decorator-icon="check" />
+    </FormKit>
   </div>
 </template>
--- src/components/SignComponents/SignUp.vue
+++ src/components/SignComponents/SignUp.vue
@@ -1,13 +1,35 @@
-<template>
-  <div>
-
-  </div>
-</template>
-
 <script setup lang="ts">
-
+import { FormKit } from '@formkit/vue'
 </script>
 
-<style scoped>
+<template>
+  <div>
+    <FormKit type="form">
+      <FormKit
+        type="email"
+        name="email"
+        label="E-mail"
+        validation="required|email"
+        validation-visibility="blur"
+      />
+      <FormKit
+        type="password"
+        name="password"
+        label="Senha"
+        value="super-secret"
+        validation="required"
+        validation-visibility="blur"
+      />
+      <FormKit
+        type="password"
+        name="password"
+        label="Confirmação de senha"
+        value="super-secret"
+        validation="required|confirm"
+        validation-visibility="blur"
+      />
 
-</style>
+      <FormKit type="checkbox" name="remember" label="Lembre-se de mim" decorator-icon="check" />
+    </FormKit>
+  </div>
+</template>
